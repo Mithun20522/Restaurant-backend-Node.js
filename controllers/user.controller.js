280d610ab030c2ab67e8e235f6a1898e9baa8dde
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-31 16:32:47 +0530

    get user api route done.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -14,3 +14,17 @@ export const getUsers = async(req, res) => {
         return res.status(500).json({message:error.message});
     }
 }
+
+export const getUser = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findById(id);
+        if(!user){
+            return res.status(404).json({message:'No user found!'});
+        }
+        return res.status(200).json(user);
+        
+    } catch (error) {
+        return res.status(500).json({message:error.message});
+    }
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUsers } from '../controllers/user.controller.js';
+import { getUser, getUsers } from '../controllers/user.controller.js';
 import { verifyToken } from '../middlewares/user.middleware.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/get-users',verifyToken,getUsers);
+userRouter.get('/get-user/:id',verifyToken,getUser);
 
 export default userRouter;
