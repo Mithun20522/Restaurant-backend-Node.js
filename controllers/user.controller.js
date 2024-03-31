f7fcb7c7e31e04ab671d30c9d55a814e5fa9a4bf
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-31 16:41:29 +0530

    user delete api route done.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -23,7 +23,24 @@ export const getUser = async(req, res) => {
             return res.status(404).json({message:'No user found!'});
         }
         return res.status(200).json(user);
+
+    } catch (error) {
+        return res.status(500).json({message:error.message});
+    }
+}
+
+export const deleteUser = async(req, res) => {
+    try {
+
+        const {id} = req.params;
+        const user = await User.findByIdAndDelete(id);
+
+        if(!user){
+            return res.status(404).json({message:'No user found!'});
+        }
         
+        return res.status(200).json({message: 'User deleted successfully.'});
+
     } catch (error) {
         return res.status(500).json({message:error.message});
     }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUser, getUsers } from '../controllers/user.controller.js';
+import { deleteUser, getUser, getUsers } from '../controllers/user.controller.js';
 import { verifyToken } from '../middlewares/user.middleware.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/get-users',verifyToken,getUsers);
-userRouter.get('/get-user/:id',verifyToken,getUser);
+userRouter.get('/get-users', verifyToken, getUsers);
+userRouter.get('/get-user/:id', verifyToken, getUser);
+userRouter.delete('/delete-user/:id', verifyToken, deleteUser);
 
 export default userRouter;
