1f7b7b5422ca03383b2bba08f8d35198c6e2702e
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-31 11:25:11 +0530

    user model done.

---
+++ models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    profileImg:{
+        type:String,
+        default: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+    },
+    userType:{
+        type:String,
+        required:true,
+        default:"customer",
+        enum:['customer','admin','vendor','rider']
+    },
+    address:{
+        type:Array,
+    },
+    phone:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+},{timestamps:true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
