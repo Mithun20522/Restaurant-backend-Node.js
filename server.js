a49aae76e918000896c1cb82010af3409a7ab173
mithun20522 <mithun20522@iiitd.ac.in>
2024-04-02 16:59:18 +0530

    port chnaged

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 connectMongoDB(MONGODB_URI);
