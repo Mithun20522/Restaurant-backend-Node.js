accf030d8bbcce428d649c450dfae3a906640f74
mithun20522 <mithun20522@iiitd.ac.in>
2024-04-02 16:28:31 +0530

    update and delete order api routes done.

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -31,3 +31,31 @@ export const getAllOrders = async(req, res) => {
         return res.status(500).json({error:error.message});
     }
 }
+
+export const updateOrder = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const order = await Order.findByIdAndUpdate(id, req.body, {new:true});
+        if(!order){
+            return res.status(404).json({message: 'No order found!'});
+        }
+        return res.status(200).json(order);
+
+    } catch (error) {
+        return res.status(500).json({error:error.message});
+    }
+}
+
+export const deleteOrder = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const order = await Order.findByIdAndDelete(id);
+        if(!order){
+            return res.status(404).json({message: 'No order found!'});
+        }
+        return res.status(200).json({message:'Order deleted successfully'});
+        
+    } catch (error) {
+        return res.status(500).json({error:error.message});
+    }
+}
--- routes/order.route.js
+++ routes/order.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createOrder, getAllOrders } from '../controllers/order.controller.js';
+import { createOrder, deleteOrder, getAllOrders, updateOrder } from '../controllers/order.controller.js';
 import { verifyToken } from '../middlewares/user.middleware.js';
 
 const orderRouter = express.Router();
 
 orderRouter.post('/create-order', createOrder);
 orderRouter.get('/get-orders', verifyToken, getAllOrders);
+orderRouter.patch('/update-order/:id', verifyToken, updateOrder);
+orderRouter.delete('/delete-order/:id', verifyToken, deleteOrder);
 
 export default orderRouter;
